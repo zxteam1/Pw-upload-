a5e17a1a2f8099fee1d9d949c5eabf96a8ce9257
creator154 <cteator898@gmail.com>
2026-04-22 12:48:49 +0530

    Change bot token in main.py
    Updated bot token for the Client instance.

--- modules/main.py
+++ modules/main.py
@@ -31,7 +31,7 @@ bot = Client(
     "bot",
     api_id=34943077,
     api_hash="11aeec678349456f1d190f02975ed89f",
-    bot_token="8605801908:AAG65hnBZlu4TeVpCSoh2TF2klGDUdWvzCI"
+    bot_token="8784602356:AAHPuhmBPoidF9VIM3Oc4Dx7Jyph_SpCPTc"
 )
 
 my_name = "ZX"
