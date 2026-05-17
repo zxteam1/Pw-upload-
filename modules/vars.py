acaf22a786b29599e1701993a8c1b25625096c66
zxteam1 <god016749@gmail.com>
2026-05-17 22:53:11 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,11 +1,7 @@
-# SudoR2spr WOODcraft
-# Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
 
 API_ID    = os.environ.get("API_ID", "11867213")
 API_HASH  = os.environ.get("API_HASH", "d475e13d8cf6937316d9fb8df5a049f9")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-#WEBHOOK = True  # Don't change this
-#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
-
+CREDIT = "@Itz_Sumit"
